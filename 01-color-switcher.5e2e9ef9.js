ae8d78e2487c5a4bb670a839797bbfc161678fc6
Classika88 <110477402+Classika88@users.noreply.github.com>
2023-01-10 11:48:05 +0000

    Deploying to gh-pages from @ Classika88/goit-js-hw-09@da5ef632847ad25da7c2a314b6ae93b41e5aeb4c 🚀

---
+++ 01-color-switcher.5e2e9ef9.js
@@ -0,0 +1,2 @@
+document.querySelector("button[data-start]"),document.querySelector("button[data-start]");
+//# sourceMappingURL=01-color-switcher.5e2e9ef9.js.map
---
+++ 01-color-switcher.7eb0f886.js
@@ -0,0 +1,2 @@
+document.querySelector("button[data-start]"),document.querySelector("button[data-start]");
+//# sourceMappingURL=01-color-switcher.7eb0f886.js.map
